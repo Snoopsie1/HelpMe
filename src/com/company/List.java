26a7f97f1f48a54ca9c530fb622d613e92649775
Snoopsie1 <oliver.rasoli@gmail.com>
2021-09-07 11:24:44 +0200

    Made Node, List and ListTest class

---
+++ src/com/company/List.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class List
+{
+    Node head = null;
+    Node tail = null;
+
+
+
+}
---
+++ src/com/company/Node.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Node
+{
+    Node prev = null;
+    Node next = null;
+
+    String data;
+
+    public Node(String data)
+    {
+        this.data = data;
+    }
+}
